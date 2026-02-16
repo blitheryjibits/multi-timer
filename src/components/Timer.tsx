f972127217f7ddd68447d1d5de3ac723952605d0
robert thornton <robert.thornton92@gmail.com>
2026-02-16 10:45:38 +0200

    Refactor - removed unused import

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useImperativeHandle, forwardRef } from 'react'
+import { useState, useEffect, useRef, useImperativeHandle, forwardRef } from 'react'
 
 type Timer = {
     duration: number
